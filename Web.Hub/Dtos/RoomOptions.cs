9787d9fff502e1f43d74cfc375e876619c116b7b
unknown <ulyanovskiy.01@mail.ru>
2022-12-11 17:50:51 +0300

    Update hub

--- Web.Hub/Dtos/RoomOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace WebHub.App.Hubs;
-
-public sealed class RoomOptions
-{
-    public string RoomId { get; init; } = string.Empty;
-
-    public string UserId { get; init; } = string.Empty;
-}
--- Web.UI/src/views/HomeView.vue
+++ Web.UI/src/views/HomeView.vue
@@ -7,64 +7,51 @@
 
 <script setup lang="ts">
 import { Peer } from 'peerjs'
-import signalR from '@microsoft/signalr'
-import signalRMsgpack from '@microsoft/signalr-protocol-msgpack'
+import { onMounted } from 'vue';
+import * as signalR from '@microsoft/signalr'
+import * as signalRMsgpack from '@microsoft/signalr-protocol-msgpack'
 
-const peer = new Peer('', {
-  host: '/',
-  port: 9000
-})
+onMounted(async () => {
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl("http://localhost:5194/room")
+    .withHubProtocol(new signalRMsgpack.MessagePackHubProtocol())
+    .build()
 
-const connection = new signalR.HubConnectionBuilder()
-  .withUrl("http://localhost:5194/room")
-  .withHubProtocol(new signalRMsgpack.MessagePackHubProtocol())
-  .build()
-
-async function start() {
-  try {
-    await connection.start();
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-start()
-
-peer.on('open', async (id: string) => {
-  await connection.invoke("Join", {
-    RoomId: 'ce620881-4dd7-418c-979c-543394147aef',
-    UserId: id
+  const peer = new Peer('', {
+    host: '/',
+    port: 9000,
+    path: '/myapp'
   })
-})
 
-const video: HTMLVideoElement | null = document.querySelector('#from')
+  await connection.start()
 
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then((camera_stream) => {
-    if (!video) return
-
-    video.srcObject = camera_stream
-
-    peer.on('call', (call) => {
-      call.answer(camera_stream)
-      call.on('stream', userVideoStream => {
-        const video: HTMLVideoElement | null = document.querySelector('#to')
-        if (video) {
-          video.srcObject = userVideoStream
-        }
-      })
-    })
-
-    connection.on("connected", (userId: any) => {
-      const call = peer.call(userId, camera_stream)
-      call.on('stream', userVideoStream => {
-        const video: HTMLVideoElement | null = document.querySelector('#to')
-        if (video) {
-          video.srcObject = userVideoStream
-        }
-      })
+  peer.on('open', async (id: string) => {
+    await connection.invoke("Join", {
+      RoomId: 'ce620881-4dd7-418c-979c-543394147aef',
+      UserId: id
     })
   })
+
+  const video: HTMLVideoElement | null = document.querySelector('#from')
+
+  const camera_stream = await navigator.mediaDevices.getUserMedia({ video: true })
+
+  video!.srcObject = camera_stream
+
+  peer.on('call', (call) => {
+    call.answer(camera_stream)
+    call.on('stream', userVideoStream => {
+      const video: HTMLVideoElement | null = document.querySelector('#to')
+      if (video) {
+        video.srcObject = userVideoStream
+      }
+    })
+  })
+
+  connection.on("connected", (userId: string) => {
+    peer.call(userId, camera_stream)
+  })
+})
 </script>
 
 <style scoped>
