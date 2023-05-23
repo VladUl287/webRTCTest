fbda65d82ce3b3f33e444b7223ed9793f274df7a
unknown <ulyanovskiy.01@mail.ru>
2023-05-23 18:04:38 +0300

    Add Web.Bus Project

---
+++ Web.Bus/Web.Bus.Common/IMessageBus.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Bus.Common;
+
+public interface IMessageBus
+{
+    Task PublishAsync<T>(T message, string exchange, CancellationToken cancellationToken = default);
+
+    Task SubscribeAsync<T>(Func<T, CancellationToken, Task> handler, string queue, CancellationToken token = default);
+}
---
+++ Web.Bus/Web.Bus.Rabbit/RabbitBus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Polly;
+using EasyNetQ;
+using Web.Bus.Common;
+using Polly.Contrib.WaitAndRetry;
+
+namespace Web.Bus.Rabbit;
+
+public class RabbitBus : IMessageBus
+{
+    private readonly IBus bus;
+    private readonly IAsyncPolicy retryPolicy;
+
+    public RabbitBus(IBus bus)
+    {
+        this.bus = bus;
+
+        retryPolicy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(10), 5)
+            // onRetry: (response, calculatedWaitDuration) =>
+            // {
+            //     this.logger.Debug(_category, "RabbitMQ Error EventBus. Check RabbitMQ Server State");
+            // }
+            );
+    }
+
+    public Task PublishAsync<T>(T message, string exchange, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SubscribeAsync<T>(Func<T, CancellationToken, Task> handler, string queue, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+}
