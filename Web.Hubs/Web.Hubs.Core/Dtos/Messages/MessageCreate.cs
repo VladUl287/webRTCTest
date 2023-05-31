b2ab60972257e164bba53a3c3f0928ea2902c55b
unknown <ulyanovskiy.01@mail.ru>
2023-05-31 18:28:23 +0300

    Remove unused Web.Hubas

--- Web.Hubs/Web.Hubs.Core/Dtos/Messages/MessageCreate.cs
+++
@@ -1,8 +0,0 @@
-namespace Web.Hubs.Core.Dtos.Messages;
-
-public sealed class MessageCreate
-{
-    public required Guid ChatId { get; init; }
-
-    public required string Content { get; init; }
-}
--- Web.Hubs/Web.Hubs.Core/Dtos/Messages/MessageUpdate.cs
+++
@@ -1,8 +0,0 @@
-namespace Web.Hubs.Core.Dtos.Messages;
-
-public sealed class MessageUpdate
-{
-    public Guid Id { get; init; }
-
-    public required string Content { get; init; }
-}
