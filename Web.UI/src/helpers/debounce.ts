5b36d3f155ec0a96342e40486f21ac898fa1d93c
unknown <ulyanovskiy.01@mail.ru>
2023-07-20 19:11:03 +0300

    Debounce update

--- Web.UI/src/helpers/debounce.ts
+++ Web.UI/src/helpers/debounce.ts
@@ -1,7 +1,22 @@
-export const debounce = (callback: (value: any) => void, delay: number = 300) => {
+// export const debounce = (callback: (value: any) => void, delay: number = 300) => {
+//     let timeout: number
+//     return (value: any) => {
+//         clearTimeout(timeout)
+//         timeout = setTimeout(() => callback(value), delay)
+//     }
+// }
+
+export function debounce<T extends (...args: any) => any>(callback: T, delay: number): (...args: Parameters<T>) => Promise<ReturnType<T>> {
     let timeout: number
-    return (value: any) => {
+
+    return (...args: Parameters<T>): Promise<ReturnType<T>> => {
         clearTimeout(timeout)
-        timeout = setTimeout(() => callback(value), delay)
+
+        return new Promise((resolve) =>
+            timeout = setTimeout(() => {
+                const result = callback(args)
+                resolve(result)
+            }, delay)
+        )
     }
 }
